d85ac08bc31a29a8f249ee1181dc3b45bf2d8951
staffonechristian <christianstaffone@gmail.com>
2017-12-11 18:56:29 -0500

    First commit

--- app/src/main/java/com/example/staffonechristian/demosql/BackgroundTask.java
+++ app/src/main/java/com/example/staffonechristian/demosql/BackgroundTask.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
